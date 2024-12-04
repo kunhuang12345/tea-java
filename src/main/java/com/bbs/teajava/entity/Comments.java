f83958aaa5fbbb6b896837519724687f28e95e5c
kunhuang <huangkunkhhuangkun@gmail.com>
2024-12-04 21:16:10 +0800

    fix: comments-添加创建时间字段

--- src/main/java/com/bbs/teajava/entity/Comments.java
+++ src/main/java/com/bbs/teajava/entity/Comments.java
@@ -1,7 +1,9 @@
 package com.bbs.teajava.entity;
 
 import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -34,6 +36,8 @@ public class Comments implements Serializable {
     private String content;
 
     @TableField("create_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createAt;
 
     @TableLogic
